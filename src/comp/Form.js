7ea169cddecd7d3e910cc3f43be700b47d9031ad
lamachina <lucasmaillard@protonmail.com>
2023-02-10 18:09:33 +0100

    apikey changes

--- src/comp/Form.js
+++ src/comp/Form.js
@@ -1,4 +1,4 @@
-import { Button, Grid, Paper, TextField, Typography } from '@mui/material';
+import { Button, CircularProgress, Grid, Paper, TextField, Typography } from '@mui/material';
 import jsPDF from 'jspdf';
 import React, { useState } from 'react'
 
@@ -7,6 +7,7 @@ function Form() {
     const [marketArea, setMarketArea] = useState('');
     const [brandName, setBrandName] = useState('');
     const [moreDetails, setMoreDetails] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
     const [rep1, setrep1] = useState("")
     //const [rep2, setrep2] = useState("")
     const [rep2f, setrep2f] = useState("")
@@ -71,6 +72,7 @@ function Form() {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        setIsLoading(true);
 
         const response = await fetch('https://api.openai.com/v1/completions', {
             method: 'POST',
@@ -96,6 +98,7 @@ function Form() {
         const generatedText = json.choices[0].text;
         setrep1(generatedText)
         console.log(generatedText);
+        setIsLoading(false);
         // do something with the generated
         //handleResponse(generatedText);
         handleDownload(generatedText)
@@ -104,7 +107,7 @@ function Form() {
     return (
         <>
             <Grid container display='flex' justifyContent='center' direction='column' justifyItems='center' alignItems='center' alignContent="center" padding='2rem' >
-                <Paper elevation={8} sx={{ padding: "3rem", width: "80%", display: "flex", justifyContent: "left" }}>
+                <Paper elevation={8} sx={{ padding: "3rem", width: "80%", display: "flex", justifyContent: "left", backgroundColor: "#FEFEFE" }}>
                     <form onSubmit={handleSubmit}>
                         <Grid p={1}>
                             <TextField
@@ -132,14 +135,19 @@ function Form() {
                                 onChange={(event) => setMoreDetails(event.target.value)}
                             />
                         </Grid>
-                        <Button type="submit" variant="contained" color="primary">
-                            Submit
-                        </Button>
+
+                        {isLoading ? <CircularProgress /> :
+                            <Button type="submit" variant="contained" color="primary">
+                                Submit
+                            </Button>}
                     </form>
-                    <Grid container direction="column" >
-                        <Typography variant='h4'>Market Overview</Typography>
-                        <Typography variant='overline'>Get you business model in 5min...</Typography>
-                        <Typography variant='body'>Fill out the informations you have and receive your business analyse in 1 click</Typography>
+                    <Grid container direction="column" display="flex" justifyContent="left" alignItems="center">
+                        <Typography color="primary" variant='h4'>Market Model Machine</Typography>
+                        <Typography pb="1rem" color="secondary" variant='overline'>your source for creative and quick market analysis</Typography>
+                        <Typography textAlign="start" width="70%" variant='body'>To get started, simply target your <span style={{ color: "#1976d2", }}> market area.</span> <br /><br />
+                            Then, enter the <span style={{ color: "#1976d2" }}>brand name</span> you'd like to focus on.<br /><br />
+                            If you have any <span style={{ color: "#1976d2" }}>additional details</span> or information that could help us analyze the target market, feel free to enter that as well.<br /><br />
+                            Once you've <span style={{ color: "green" }}>submitted</span> your information, our app will automatically generate a report. When downloading is proceeding, do not quit the page.</Typography>
                         {/* <Button onClick={handleDownload}>Download PDF</Button> */}
                     </Grid>
                 </Paper>
