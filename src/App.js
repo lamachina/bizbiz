8b5bcdae4daf782ff3c02e66ac13e5076e9d900b
lamachina <lucasmaillard@protonmail.com>
2023-02-11 15:10:33 +0100

    apikey changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import { Button, ThemeProvider } from '@mui/material';
 import './App.css';
 import Form from './comp/Form';
+import { useState } from 'react';
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0c343d  '
+    },
+    secondary: {
+      main: '#45818e'
+    },
+  }
+});
 
 function App() {
+  const [ModusB, setModusB] = useState(true)
+
+  const handleClick = () => {
+    ModusB ?
+      setModusB(false) :
+      setModusB(true)
+
+
+  }
+
   return (
-    <div className="App">
-      <Form />
+    <div className={ModusB ? "App" : "App dark"}>
+      <ThemeProvider theme={theme}>
+        <Button variant='contained' color='secondary' onClick={handleClick}>
+          {ModusB ? "pass in dark mode" : "pass in light mode"}
+        </Button>
+        <Form />
+      </ThemeProvider>
     </div>
   );
 }
